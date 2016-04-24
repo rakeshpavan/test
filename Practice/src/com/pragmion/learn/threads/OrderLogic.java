1eef0cec7cfa34dbd626039e1c7a6961d090d6b1
Rakesh Lagadapati <rakeshpavan@hotmail.com>
2016-04-24 11:12:13 -0500

    Updated with serviceAA call and capturing responses

--- Practice/src/com/pragmion/learn/threads/OrderLogic.java
+++ Practice/src/com/pragmion/learn/threads/OrderLogic.java
@@ -2,6 +2,7 @@ package com.pragmion.learn.threads;
 
 public class OrderLogic {
 
+	String resA, resAA, resB;
 	public static void main(String[] args) {
 		OrderLogic ol = new OrderLogic();
 		ol.processOrder();
@@ -17,6 +18,7 @@ public class OrderLogic {
 		Thread tA = new Thread() {
 			public void run() {
 				serviceA();
+				serviceAA(resA);
 			}
 		};
 		
@@ -42,7 +44,7 @@ public class OrderLogic {
 		// call Service C{
 		// output of service AA && output Service B is input for Service C
 		// }
-		serviceC();
+		serviceC(resAA, resB);
 
 	}
 
@@ -57,6 +59,23 @@ public class OrderLogic {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		resA = "A";
+	}
+	
+	public void serviceAA(String pStr) {
+		System.out.println("Input String to AA:"+pStr);
+		
+		try {
+			System.out.println("Inside serviceAA() -1");
+			Thread.sleep(100);
+			System.out.println("Inside serviceAA() -2");
+			Thread.sleep(100);
+			System.out.println("Inside serviceAA() -3");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		resAA = "AA";
 	}
 
 	public void serviceB() {
@@ -69,9 +88,13 @@ public class OrderLogic {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		resB = "B";
 	}
 
-	public void serviceC() {
+	public void serviceC(String a, String b) {
+		System.out.println("Inside Service C:"+a);
+		System.out.println("Inside Service C:"+b);
 		System.out.println("Inside serviceC()");
 	}
 
@@ -79,21 +102,34 @@ public class OrderLogic {
 
 /*
 Test1:
-Inside serviceA() -1
 Inside serviceB() -1
-Inside serviceB() -2
+Inside serviceA() -1
 Inside serviceA() -2
+Inside serviceB() -2
 Inside serviceB() -3
 Inside serviceA() -3
+Input String to AA:A
+Inside serviceAA() -1
+Inside serviceAA() -2
+Inside serviceAA() -3
+Inside Service C:AA
+Inside Service C:B
 Inside serviceC()
 
+
 Test2:
-Inside serviceA() -1
 Inside serviceB() -1
-Inside serviceA() -2
+Inside serviceA() -1
 Inside serviceB() -2
-Inside serviceB() -3
+Inside serviceA() -2
 Inside serviceA() -3
+Inside serviceB() -3
+Input String to AA:A
+Inside serviceAA() -1
+Inside serviceAA() -2
+Inside serviceAA() -3
+Inside Service C:AA
+Inside Service C:B
 Inside serviceC()
 
 Test3:
@@ -101,8 +137,14 @@ Inside serviceA() -1
 Inside serviceB() -1
 Inside serviceA() -2
 Inside serviceB() -2
-Inside serviceA() -3
 Inside serviceB() -3
+Inside serviceA() -3
+Input String to AA:A
+Inside serviceAA() -1
+Inside serviceAA() -2
+Inside serviceAA() -3
+Inside Service C:AA
+Inside Service C:B
 Inside serviceC()
 
 *
